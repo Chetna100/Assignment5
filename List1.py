1b9d4ec4a4a441829dfb1ad18fa849901236b694
Chetna Patel <99310118+Chetna100@users.noreply.github.com>
2025-07-30 21:22:19 +0530

    Add files via upload

---
+++ List1.py
@@ -0,0 +1,7 @@
+List = [1,2,3,4,5,6,7,8,9,10]
+print("Original List: ", List)
+
+Extracted_List = List[:5]
+print("Extracted first five Elements: ", Extracted_List)
+reversed_List = Extracted_List[::-1]
+print("Reversed Extracted list: ", reversed_List)
